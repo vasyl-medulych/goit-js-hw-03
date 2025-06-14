1a4ca380c3a4b5f3ffcd6380045975697dd891b7
Vasyl Linkin <linkinmvv@gmail.com>
2025-06-14 13:57:07 +0300

    tasks complate

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,12 @@
-// Задача 1. Замовлення дроїдів
-
-'use strict';
-function makeTransaction(quantity, pricePerDroid) {
-  let totalPrice = quantity * pricePerDroid;
-  return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+('use strict');
+console.log('Задача 1. Генератор slug');
+function slugify(title) {
+  const slug = title.split(' ');
+  return slug.join('-').toLowerCase();
 }
 
-console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
-console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
-console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+console.log(slugify('Arrays for beginners')); // "arrays-for-beginners"
+console.log(slugify('English for developer')); // "english-for-developer"
+console.log(slugify('Ten secrets of JavaScript')); // "ten-secrets-of-javascript"
+console.log(slugify('How to become a JUNIOR developer in TWO WEEKS')); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log('_____________________________________________');
--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,17 @@
-// Задача 3. Ширина елемента
-
 'use strict';
-function getElementWidth(content, padding, border) {
-  const totalWidth =
-    parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
-  return totalWidth;
+console.log('Задача 3. Фільтрація масиву чисел');
+function filterArray(numbers, value) {
+  const result = [];
+  for (const number of numbers) {
+    if (number > value) {
+      result.push(number);
+    }
+  }
+  return result;
 }
 
-console.log(getElementWidth('50px', '8px', '4px')); // 74
-console.log(getElementWidth('60px', '12px', '8.5px')); // 101
-console.log(getElementWidth('200px', '0px', '0px')); // 200
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
