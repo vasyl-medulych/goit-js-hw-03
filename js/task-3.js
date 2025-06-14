6d455ab29288aeaacaa64cc010aeed4042bcb1d7
Vasyl Medulych <linkinmvv@gmail.com>
2025-06-14 12:35:18 +0300

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,12 @@
+// Задача 3. Ширина елемента
+
+'use strict';
+function getElementWidth(content, padding, border) {
+  const totalWidth =
+    parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
+  return totalWidth;
+}
+
+console.log(getElementWidth('50px', '8px', '4px')); // 74
+console.log(getElementWidth('60px', '12px', '8.5px')); // 101
+console.log(getElementWidth('200px', '0px', '0px')); // 200
